6c74ee0bc4597e2e56be366607afcc1fb6047fd2
Vladan Babic <vbabic@resourcepos.com>
2023-04-23 23:35:11 +0200

    Add light bulb svg as a switch to change color mode

--- projects/Sign-up form/JS/mainScript.js
+++ projects/Sign-up form/JS/mainScript.js
@@ -7,33 +7,36 @@
 let toggle = document.querySelector(".toggle");
 // let checked = toggle.checked;
 
-
 toggle.addEventListener("change", () => {
   checked = toggle.checked;
   console.log(checked);
+  // create img for light on
+  // let img = document.createElement("img");
+  // img.src = "../img/light-bulb-on.svg";
+
   if (checked) {
-    document.querySelector(".toggle-label").textContent = "Light mode";
+    document.querySelector(".light-svg").src = "../img/light-bulb-on.svg";
   } else {
-    document.querySelector(".toggle-label").textContent = "Dark mode";
+    document.querySelector(".light-svg").src = "../img/light-bulb-off.svg";
   }
-  checked ? updateColor("#391818", "#eefbfb","#1c3673") : updateColor("#f6f7f8", "#5d6063","#d7dee3");
+  checked
+    ? updateColor("#391818", "#eefbfb", "#1c3673")
+    : updateColor("#f6f7f8", "#5d6063", "#d7dee3");
 });
 
-
-
-
 // updateColor function
-function updateColor(backgroundColor, txtColor,bodyBgColor) {
+function updateColor(backgroundColor, txtColor, bodyBgColor) {
   document.documentElement.style.setProperty(
     "--form-background-color",
     backgroundColor
   );
   document.documentElement.style.setProperty("--font-color", txtColor);
-  document.documentElement.style.setProperty("--body-background-color", bodyBgColor);
-
+  document.documentElement.style.setProperty(
+    "--body-background-color",
+    bodyBgColor
+  );
 }
 
 // updateColor("#f6f7f8", "#5d6063")
 
-// signup 
-
+// signup
